40df6a61a82bb4ac798f01bf370b371793995f97
user <vaamfaf@gmail.com>
2024-10-17 13:08:17 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,3 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Triangle triangle = new Triangle();
-        triangle.a = scanner.nextInt();
-        triangle.b = scanner.nextInt();
-        triangle.c = scanner.nextInt();
-        System.out.print("Ayanty : ");
-        triangle.area();
-    }
 }
